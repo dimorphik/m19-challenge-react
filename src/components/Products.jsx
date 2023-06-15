ec6c0dfc5c9fcb6e2baa5236a0c926ec57f1da03
dimorphik <dimorphik@gmail.com>
2023-06-14 22:54:10 -0700

    pointless commit

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 const Products = (props) => {
   useEffect(() => {
-    document.title = "Gulp Roast Coffee - Products";
+    document.title = "Gulp Roast Coffee : Products";
   }, []);
 
   return (
