be22e1795a77ad79c1dbd85203d3fef5714078ab
dimorphik <dimorphik@gmail.com>
2023-06-14 23:18:34 -0700

    fixed logo image in deployment

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ const Logo = (props) => {
   return (
     <li className="logo">
       <NavLink to="/">
-        <img src="/media/images/gulp-logo-light.png" alt="Gulp Roast. Click for home." />
+        <img src="./media/images/gulp-logo-light.png" alt="Gulp Roast. Click for home." />
       </NavLink>
     </li>
   );
