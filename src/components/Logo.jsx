1374b1bb447bd2250ddc6d0d4cb5f888d0ff5a2b
dimorphik <dimorphik@gmail.com>
2023-06-14 23:44:20 -0700

    fixed logo link

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 const Logo = (props) => {
   return (
     <li className="logo">
-      <NavLink to="/">
+      <NavLink to="./">
         <img
           src={`${process.env.PUBLIC_URL}/media/images/gulp-logo-light.png`}
           alt="Gulp Roast. Click for home."
